db7f5fbe5baf5f982eccfacef135a79da39e33cd
Keito <cait@KeitonoMacBook-Air.local>
2022-02-16 10:51:17 +0900

    Add habits controller

---
+++ app/controllers/api/v1/habits_controller.rb
@@ -0,0 +1,58 @@
+module Api
+  module V1
+    class HabitsController < ApplicationController
+      def index
+        habits = Habit.active
+        render json: {
+          habits: habits
+        }, status: :ok
+      end
+
+      def create
+        user = User.find_by(email: params[:email])
+        habit = Habit.new(
+          user_id: user.id,
+          title: params[:title],
+          detail: params[:detail],
+          active: true
+        )
+        if habit.save
+          render json: {
+            habit: habit
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      def show
+        habit = Habit.find(params[:id])
+        render json: {
+          habit: habit
+        }, status: :ok
+      end
+
+      def update
+        habit = Habit.find(params[:id])
+        if habit.update(title: params[:title])
+          render json: {
+            habit: habit
+          }, status: :created
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+
+      def destroy
+        habit = Habit.find(params[:id])
+        if habit.update(active: false)
+          render json: {
+            habit: habit
+          }, status: :ok
+        else
+          render json: {}, status: :internal_server_error
+        end
+      end
+    end
+  end
+end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,4 +3,6 @@ class Habit < ApplicationRecord
   validates :title, :detail, :count, presence: true
   validates :title, length: { maximum: 20 }
   validates :count, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :active, -> { where(active: true) }
 end
